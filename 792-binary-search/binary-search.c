1f7516429b6c257d71ea8ab1ca61a853c52e7b0a
harish  chappati <harish18chappati@gmail.com>
2026-08-27 18:56:29 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (44.74%) - LeetSync

---
+++ 792-binary-search/binary-search.c
@@ -0,0 +1,19 @@
+int search(int* nums, int numsSize, int target) {
+    int low=0;int high=numsSize-1;
+    while(low<high+1)
+    {
+    
+    int mid=(low+high)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(target>nums[mid])
+        {
+        low=mid+1;    
+        }
+        else{
+
+            high=mid-1;
+        }}
+        return -1;
+}
